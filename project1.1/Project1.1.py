1315c310f05c8fe90cac79e0a74613350f1e5323
mingyugai <mingyugai@gmail.com>
2017-03-31 21:39:08 -0700

    add comments

--- project1.1/Project1.1.py
+++ project1.1/Project1.1.py
@@ -1,18 +1,19 @@
-#path = "/home/mingyugai/Project1_1/"
-path = "/Users/mingyugai/Downloads/files/"
-
 import os
 
+path = "/Users/mingyugai/Downloads/files/"
+#read all files in the directory
 def readAllFiles():
     for root, dirs, files in os.walk(path):
         file_list = files
     return file_list
 
+#select the line start with "en" and also has no suffix
 def selectEnstart(line):
     if line.startswith('en') and len(line.split(' ')[0]) == 2:
         return True
     return False
 
+#select line that the page title not start with such string
 def selectTitle(line):
     temp = ['Media:', 'Special:', 'Talk:', 'User:', 'User_talk:','Project:',
             'Project_talk:','File:','File_talk:','MediaWiki:','MediaWiki_talk:',
@@ -23,11 +24,13 @@ def selectTitle(line):
             return False
     return True
 
+#select line that page title not start with lower case
 def selectFirstTitle(line):
     if line.split(' ')[1][0].islower() is True:
         return False
     return True
 
+#select line that page title not end with such format
 def selectFormat(line):
     temp = ['.jpg', '.gif', '.png', '.JPG', '.GIF', '.PNG', '.txt', '.ico']
     for str in temp:
@@ -35,6 +38,7 @@ def selectFormat(line):
             return False
     return True
 
+#select line that page title not have such string
 def selectBoilerplate(line):
     temp = ['404_error/', 'Main_Page', 'Hypertext_Transfer_Protocol', 'Search']
     for str in temp:

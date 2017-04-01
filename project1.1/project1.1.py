8fcd20a1dcc57c1273c1b3b00c88aa478928680f
mingyugai <mingyugai@gmail.com>
2017-04-01 14:32:39 -0700

    update standard code

--- project1.1/project1.1.py
+++ project1.1/project1.1.py
@@ -1,97 +1,81 @@
 import os
+import argparse
 
-#TODO any static varable use capital letter.eg PATH=...
-path = "/Users/mingyugai/Downloads/files/"
 #read all files in the directory
-#TODO any internal function (which not be used outside of the package), start with "_".
-#TODO eg. _readAllFiles()
-def readAllFiles():
-    for root, dirs, files in os.walk(path):
-        file_list = files
+def _read_all_files(input_path):
+    file_list = []
+    for root, dirs, files in os.walk(input_path):
+        for file in files:
+            if file.startswith('pagecount'):
+                file_list.append(file)
     return file_list
 
 #select the line start with "en" and also has no suffix
-def selectEnstart(line):
-#TODO, you do not use if. You can directly return this condition
-    if line.startswith('en') and len(line.split(' ')[0]) == 2:
-        return True
-    return False
+def _select_enstart(line):
+    return line.startswith('en') and len(line.split(' ')[0]) == 2
 
 #select line that the page title not start with such string
-def selectTitle(line):
+def _select_title(line):
     temp = ['Media:', 'Special:', 'Talk:', 'User:', 'User_talk:','Project:',
             'Project_talk:','File:','File_talk:','MediaWiki:','MediaWiki_talk:',
             'Template:','Template_talk:','Help:','Help_talk:','Category:',
             'Category_talk:','Portal:','Wikipedia:','Wikipedia_talk:']
+    split = line.split(' ')
     for str in temp:
-        #TODO, you do split for multiple times which equals the length of temp. You can just do once
-        #TODO, before for loop.
-        if line.split(' ')[1].startswith(str):
+        if split[1].startswith(str):
             return False
     return True
 
 #select line that page title not start with lower case
-def selectFirstTitle(line):
-    #TODO do not need if condition
-    if line.split(' ')[1][0].islower() is True:
-        return False
-    return True
+def _select_first_title(line):
+    return line.split(' ')[1][0].islower()
 
 #select line that page title not end with such format
-def selectFormat(line):
+def _select_format(line):
     temp = ['.jpg', '.gif', '.png', '.JPG', '.GIF', '.PNG', '.txt', '.ico']
-    #TODO split out of for, just do once.
+    split = line.split(' ')
     for str in temp:
-        if line.split(' ')[1][-4:-1] in temp:
+        if split[1][-4:-1] in temp:
             return False
     return True
 
 #select line that page title not have such string
-def selectBoilerplate(line):
+def _select_boilerplate(line):
     temp = ['404_error/', 'Main_Page', 'Hypertext_Transfer_Protocol', 'Search']
-    #TODO same thing
+    split = line.split(' ')
     for str in temp:
-        if line.split(' ')[1].find(str) is True:
+        if split[1].find(str) is True:
             return False
     return True
-# TODO, normally, if __name__, we do not do such business logic. In __main__, we only part the
-# TODO, parameter of application input. For your script, there is no parameter. So just call one
-# TODO, main(). And in that function, you can do some business logic. 
-if __name__ == '__main__':
-    file_list = readAllFiles()
-    newfile = []
-    for file in file_list:
-        #TODO, put such condition when call readAllFiles()
-        if file.startswith('page'):
-            with open(path + file) as f:
-                doc =f.readlines()
-                for line in doc:
-                    if selectEnstart(line) and selectTitle(line) and selectFirstTitle(line) \
-                            and selectFormat(line) and selectBoilerplate(line):
-                        newline = "{}\t{}".format(line.split(' ')[1], line.split(' ')[2])
-                        #TODO, change the newfile name, which is confusing. eg. newFileList
-                        newfile.append(newline)
-    #TODO, delete unnecessary print
-    print newfile
-    #TODO, why keep the try part, delete those debug info. 
-    for x in newfile:
-        try:
-            int(x.split('\t')[1])
-        except Exception:
-            print x
-    newfile.sort(key = lambda x: int(x.split('\t')[1]))
-    #TODO, you can write one sub fuction for this logic.
-    with open('/Users/mingyugai/Downloads/' + 'testfile.txt', 'w') as f:
-        for s in newfile:
-            print s
+
+#output the file
+def _write_files(out_path, out_file_list, name):
+    with open(out_path + name, 'w') as f:
+        for s in out_file_list:
             f.write(s + '\n')
-#TODO: Name convention in python may be different from java. So you need to keep in mind what
-#TODO: should be like in python
-#TODO: class: class ThisIsAClass
-#TODO: function: def this_is_a_function
-#TODO: variable in fucntion: this_is_a_var
-#TODO: variable with a static value: THIS_IS_THIS_YEAR=2017, THIS_IS_MONTH=['Jan', 'Feb',....]
-#TODO: private/internal function: _this_is_a_private_function()
+
+def main(args):
+    file_list = _read_all_files(args.input_path)
+    newfile_list = []
+    for file in file_list:
+        # TODO, put such condition when call readAllFiles()
+        with open(args.input_path + file) as f:
+            doc = f.readlines()
+            for line in doc:
+                if _select_enstart(line) and _select_title(line) and _select_first_title(line) \
+                        and _select_format(line) and _select_boilerplate(line):
+                    newline = "{}\t{}".format(line.split(' ')[1], line.split(' ')[2])
+                    newfile_list.append(newline)
+    newfile_list.sort(key=lambda x: int(x.split('\t')[1]))
+    _write_files(args.out_path, newfile_list, args.name)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-path",  help="input file path")
+    parser.add_argument("-o", "--out-path", help="output file path")
+    parser.add_argument("-n", "--name",help="file name of output")
+    args = parser.parse_args()
+    main(args)
 
 
 
